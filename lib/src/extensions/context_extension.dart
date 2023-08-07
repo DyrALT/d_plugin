c90ea59dd7fef14ab0fa8357a75c609bd3bd0c9e
DyrALT <dyr.altunakar@gmail.com>
2023-08-07 12:53:55 +0300

    The height and width properties were added to the context extension.

--- lib/src/extensions/context_extension.dart
+++ lib/src/extensions/context_extension.dart
@@ -6,4 +6,19 @@ import 'package:flutter/widgets.dart';
 /// app.
 extension NavigatorMansager on BuildContext {
   NavigatorManager get navigation => NavigatorManager.of(this);
+
+  /// The function calculates the height of a widget based on a given value.
+  /// 
+  /// Args:
+  ///   value (double): The value is a double that represents the multiplier for the height of the
+  /// screen.
+  double height(double value) => MediaQuery.of(this).size.height * value;
+
+  /// The function calculates the width of a widget based on a given value and the device's screen
+  /// width.
+  /// 
+  /// Args:
+  ///   value (double): The value is a double that represents a multiplier for the width of the current
+  /// device's screen.
+  double width(double value) => MediaQuery.of(this).size.width * value;
 }
