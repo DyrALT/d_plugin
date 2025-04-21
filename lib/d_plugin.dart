9cedf7906b465041d0ca698431b4b5c0efd7a067
DyrALT <dyr.altunakar@gmail.com>
2025-04-21 20:37:49 +0300

    add export for value_notifier_extension to enhance reactive programming support

--- lib/d_plugin.dart
+++ lib/d_plugin.dart
@@ -3,6 +3,7 @@ library d_plugin;
 
 export 'src/extensions/context_extension.dart';
 export 'src/extensions/string_extenison.dart';
+export 'src/extensions/value_notifier_extension.dart';
 export 'src/mixin/cubit_mixin/cubit_mixin.dart';
 export 'src/utils/json_manager/json_manager.dart';
 export 'src/utils/navigator/navigator_manager.dart';
